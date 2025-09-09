fbf6c895360742e45bb773a928500ec80467494b
marcus <wendpanga1004@gmail.com>
2025-09-09 19:50:24 +0000

    'all'

--- enrollment_summary/filters.py
+++ enrollment_summary/filters.py
@@ -2,7 +2,7 @@ import django_filters
 from student.models import CourseEnrollment
 
 class EnrollmentSummaryFilter(django_filters.FilterSet):
-    user_id = django_filters.NumberFilter(field_name="user__id")
+    user_id = django_filters.NumberFilter(field_name="user_id")
     active = django_filters.BooleanFilter(field_name="is_active")
 
     class Meta:
